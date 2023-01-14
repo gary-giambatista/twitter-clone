0048f21b6a30298d748d3ba765f87240df6c272d
Gary Giambatista <gary.giambatista@gmail.com>
2023-01-14 00:25:04 -0500

    implemented and successfully tested Twitter auth and next-auth. Added logic based conditional rendering and onClick for the Sidebar > SidebarRow components, which passes an onClick function, which in this case is used for sign-in/out.

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,12 +8,15 @@ import {
 	RectangleStackIcon,
 	UserIcon,
 } from "@heroicons/react/24/outline";
+import { signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import React from "react";
 import TwitterLogo from "../public/twitter-logo.png";
 import SidebarRow from "./SidebarRow";
 
 export default function Sidebar() {
+	const { data: session } = useSession();
+
 	return (
 		<div className="flex flex-col col-span-2 items-center px-4 md:items-start">
 			<Image src={TwitterLogo} alt="twitter" width="40" className="m-3" />
@@ -23,7 +26,11 @@ export default function Sidebar() {
 			<SidebarRow Icon={EnvelopeIcon} title="Messages" />
 			<SidebarRow Icon={BookmarkIcon} title="Bookmarks" />
 			<SidebarRow Icon={RectangleStackIcon} title="Lists" />
-			<SidebarRow Icon={UserIcon} title="Sign In" />
+			<SidebarRow
+				onClick={session ? signOut : signIn}
+				Icon={UserIcon}
+				title={session ? "Sign Out" : "Sign In"}
+			/>
 			<SidebarRow Icon={EllipsisHorizontalCircleIcon} title="More" />
 		</div>
 	);
