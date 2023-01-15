10e176718543e5c222170c2c3f35f028afcd92ee
Gary Giambatista <gary.giambatista@gmail.com>
2023-01-15 00:35:39 -0500

    trying @ts-nocheck

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -1,8 +1,9 @@
+//@ts-nocheck
 import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
     projectId: 'h8b3v6e1',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
