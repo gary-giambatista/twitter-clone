d99659100c4a1a318933501189f87e34e287334a
Gary Giambatista <gary.giambatista@gmail.com>
2023-01-14 20:06:01 -0500

    updated fetchTweets to use local env. route

--- utils/fetchTweets.ts
+++ utils/fetchTweets.ts
@@ -2,7 +2,7 @@ import { Tweet } from "../typings";
 
 export const fetchTweets = async () => {
 	//rest api call to our backend (pages/api/getTweets.ts)
-	const res = await fetch("http://localhost:3000/api/getTweets");
+	const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getTweets`);
 
 	const data = await res.json();
 	const tweets: Tweet[] = data.tweets;
