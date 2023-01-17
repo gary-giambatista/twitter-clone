642d2e6bff45b14e1d152a22fd29c14af433bdf4
Gary Giambatista <gary.giambatista@gmail.com>
2023-01-17 00:41:29 -0500

    updated types in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,16 @@
+import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+function MyApp({
+	Component,
+	pageProps,
+}: AppProps<{
+	session: Session;
+}>) {
 	return (
-		<SessionProvider session={session}>
+		<SessionProvider session={pageProps.session}>
 			<Component {...pageProps} />
 		</SessionProvider>
 	);
