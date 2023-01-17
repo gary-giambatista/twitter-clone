d22319a85a8f571fc355f6e0d7d13236fa65e653
Gary Giambatista <gary.giambatista@gmail.com>
2023-01-17 00:59:50 -0500

    fixed out of sync files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,10 @@
-import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 
-function MyApp({
-	Component,
-	pageProps,
-}: AppProps<{
-	session: Session;
-}>) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
 	return (
-		<SessionProvider session={pageProps.session}>
+		<SessionProvider session={session}>
 			<Component {...pageProps} />
 		</SessionProvider>
 	);
